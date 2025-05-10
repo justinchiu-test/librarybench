26a6f2dfa2d60a6b98221ae7c56ffabfc6e70a2a
justinchiu-cohere <justinchiu@cohere.com>
2025-05-09 20:39:22 -0400

    rename to personas

--- scripts/synth_ideas.sh
+++ scripts/synth_ideas.sh
@@ -1,1 +1,3 @@
+# This can be run multiple times to generate 10 ideas at a time
 claude --dangerously-skip-permissions -p "Follow the instructions in prompts/ideas.txt. If there are existing project ideas in ideas/*, make sure your new project ideas are different."
+
---
+++ scripts/synth_personas.sh
@@ -0,0 +1,2 @@
+# This will generate personas for all ideas at once
+claude --dangerously-skip-permissions -p "Follow the instructions in prompts/personas.txt. Be sure to generate personas for every idea. Do not stop until finished"
